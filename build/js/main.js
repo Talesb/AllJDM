ff11c2b38d8fe06ae703d62fc698c9a33a5ebaf9
Talesb <talesbatistafig@hotmail.com>
2018-12-29 20:58:57 -0200

    adjusting images

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,43 @@
- 
+
+$(document).ready(function () {
+
+    setActivePage();
+
+
+});
+
+
+function setActivePage() {
+    var link = location.pathname.split("/")
+
+    if (link.length<3) {
+        $("#homeAnchor").addClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+
+    }else{
+    if (link[2].includes('about')) {
+        $("#aboutAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+    }
+    if (link[2].includes('project')) {
+        $("#projectAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+    }
+    if (link[2].includes('contact')) {
+        $("#contactAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+    }}
+
+
+}
 // ===== Scroll to Top ==== 
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,40 @@
- 
+
+$(document).ready(function () {
+
+    setActivePage();
+
+
+});
+
+
+function setActivePage() {
+    var link = location.pathname.split("/")
+
+    if (link.length<3) {
+        $("#homeAnchor").addClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+
+    }else{
+    if (link[2].includes('about')) {
+        $("#aboutAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+    }
+    if (link[2].includes('project')) {
+        $("#projectAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#contactAnchor").removeClass("text-dark");
+    }
+    if (link[2].includes('contact')) {
+        $("#contactAnchor").addClass("text-dark");
+        $("#homeAnchor").removeClass("text-dark");
+        $("#aboutAnchor").removeClass("text-dark");
+        $("#projectAnchor").removeClass("text-dark");
+    }}
+
+
+}
