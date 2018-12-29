4956b134482156f6e5b0077a34b9b1c1e4ac6c69
Talesb <talesbatistafig@hotmail.com>
2018-12-29 21:10:16 -0200

    Adjusting js build folder

--- build/js/main.js
+++ build/js/main.js
@@ -10,31 +10,32 @@ $(document).ready(function () {
 function setActivePage() {
     var link = location.pathname.split("/")
 
-    if (link.length<3) {
+    if (link.length < 3 || link[2].includes('index')) {
         $("#homeAnchor").addClass("text-dark");
         $("#aboutAnchor").removeClass("text-dark");
         $("#projectAnchor").removeClass("text-dark");
         $("#contactAnchor").removeClass("text-dark");
 
-    }else{
-    if (link[2].includes('about')) {
-        $("#aboutAnchor").addClass("text-dark");
-        $("#homeAnchor").removeClass("text-dark");
-        $("#projectAnchor").removeClass("text-dark");
-        $("#contactAnchor").removeClass("text-dark");
+    } else {
+        if (link[2].includes('about')) {
+            $("#aboutAnchor").addClass("text-dark");
+            $("#homeAnchor").removeClass("text-dark");
+            $("#projectAnchor").removeClass("text-dark");
+            $("#contactAnchor").removeClass("text-dark");
+        }
+        if (link[2].includes('project')) {
+            $("#projectAnchor").addClass("text-dark");
+            $("#homeAnchor").removeClass("text-dark");
+            $("#aboutAnchor").removeClass("text-dark");
+            $("#contactAnchor").removeClass("text-dark");
+        }
+        if (link[2].includes('contact')) {
+            $("#contactAnchor").addClass("text-dark");
+            $("#homeAnchor").removeClass("text-dark");
+            $("#aboutAnchor").removeClass("text-dark");
+            $("#projectAnchor").removeClass("text-dark");
+        }
     }
-    if (link[2].includes('project')) {
-        $("#projectAnchor").addClass("text-dark");
-        $("#homeAnchor").removeClass("text-dark");
-        $("#aboutAnchor").removeClass("text-dark");
-        $("#contactAnchor").removeClass("text-dark");
-    }
-    if (link[2].includes('contact')) {
-        $("#contactAnchor").addClass("text-dark");
-        $("#homeAnchor").removeClass("text-dark");
-        $("#aboutAnchor").removeClass("text-dark");
-        $("#projectAnchor").removeClass("text-dark");
-    }}
 
 
 }
